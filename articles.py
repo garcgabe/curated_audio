8cf668bec9e362e565b99ac8d3e6621db0547218
garcgabe <garciagabe0033@yahoo.com>
2024-01-29 18:11:30 -0500

    articles

---
+++ articles.py
@@ -0,0 +1,11 @@
+import requests
+## import beautifulsoup
+
+
+if __name__=="__main__":
+    # use beautifulsoup to connect to article
+    # maybe parse all articles somehow? 
+    # -- getArticles based on html tags <font> 
+    base_url = "paulgraham.com"
+
+    # add on links to base_url and parse through text  
