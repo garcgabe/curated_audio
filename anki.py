eaa3e2a539026b4928f238b098d5058402a457a3
garcgabe <garciagabe0033@yahoo.com>
2024-01-29 16:24:12 -0500

    got card info; gonna take awhile to get all card info

--- anki.py
+++ anki.py
@@ -10,14 +10,13 @@ if __name__=="__main__":
     # -- findCards, params = "deck": "name"
     # then get all the card data for these cards
     # -- cardsInfo, params = "cards": [ids as ints]
-    all_card_ids = []
-
-    action = "findCards"
-    params = { "query": "*" }
-
-    print(params)
-
-    response = requests.post('http://127.0.0.1:8765', json={'action': action, 'params': params, 'version': 6})
+ 
+    response = requests.post('http://127.0.0.1:8765', json={'action': "findCards", 'params': {"query": "*"}, 'version': 6})
     print(response.status_code)
     response_data = response.json()
-    print(json.dumps(response.data.items(), indent=4) )
+    all_card_ids = response_data['result']
+
+    card_info_response = requests.post('http://127.0.0.1:8765', json={'action': "cardsInfo", 'params': {"cards": all_card_ids}, 'version': 6})
+    print(response.status_code)
+    card_info_response_data = response.json()
+    print(card_info_response_data.items())
